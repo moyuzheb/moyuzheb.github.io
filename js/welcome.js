0a9714c36168e52c72ed6add5506a4e41f3a0e27
moyuzheb <1329857597@qq.com>
2023-02-01 18:37:39 +0800

    Site updated: 2023-02-01 18:37:37

--- js/welcome.js
+++ js/welcome.js
@@ -2,7 +2,7 @@ function welcome(){
     let welcome_text = '整了个弹窗，牛逼吧（）'
     
     swal({
-        title: " 欢迎光顾qwq~",
+        title: " 欢迎光临qwq~",
         text: welcome_text+'\n不要问我为什么挂了Transgender Flag，问就是尊重',
         text: welcome_text+'\n打开页面下方音乐以获得更佳体验',
         text: welcome_text+'\nSite updated:2/1',
