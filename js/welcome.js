26bf1fd10cffa3db5338ce3e54773e1010390316
moyuzheb <1329857597@qq.com>
2023-02-01 18:32:47 +0800

    Site updated: 2023-02-01 18:32:43

--- js/welcome.js
+++ js/welcome.js
@@ -1,13 +1,12 @@
 function welcome(){
     let welcome_text = '整了个弹窗，牛逼吧（）'
-    if(document.referrer!==''){
-        let referrer=document.referrer.split("/")[2];
-        welcome_text="欢迎你，来自"+referrer.toUpperCase()+"的用户！";
-        if(referrer.toUpperCase()==document.domain.toUpperCase())return;
-    }
+    
     swal({
         title: " 欢迎光顾qwq~",
-        text: welcome_text+'\n打开页面下方音乐以获得更佳体验！',
+        text: welcome_text+'\n不要问我为什么挂了Transgender Flag，问就是尊重',
+        text: welcome_text+'\n打开页面下方音乐以获得更佳体验',
+        text: welcome_text+'\nSite updated:2/1',
+        text: welcome_text+'\n本来想写个pixiv日榜接口的，贼难，不如弃 暗 投 明，直接用360壁纸的api',
         imageUrl: "/immm/flag.jpg",
         timer: 3000,
         showConfirmButton: false
