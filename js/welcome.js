902158f9d5969db331099150c5d5ce7e82c111cb
moyuzheb <1329857597@qq.com>
2023-02-05 13:40:11 +0800

    Site updated: 2023-02-05 13:40:05

--- js/welcome.js
+++ js/welcome.js
@@ -2,16 +2,12 @@ function welcome(){
     let welcome_text = '整了个弹窗，牛逼吧（）'
     if(document.referrer!==''){
         let referrer=document.referrer.split("/")[2];
-        welcome_text="整了个弹窗，牛逼吧（）";
+        welcome_text="From"+referrer.toUpperCase();
         if(referrer.toUpperCase()==document.domain.toUpperCase())return;
     }
-    
     swal({
         title: " 欢迎光临qwq~",
-        text: welcome_text+'\n不要问我为什么挂了Transgender Flag，问就是尊重',
-        text: text+'\n打开页面下方音乐以获得更佳体验',
-        text: text+'\n Site updated:2/1',
-        text: text+'\n本来想写个pixiv日榜接口的，贼难，不如弃 暗 投 明，直接用360壁纸的api',
+        text: welcome_text+'\n不要问我为什么挂了Transgender Flag，问就是尊重\n打开页面下方音乐以获得更佳体验\nSite updated:2/1\n本来想写个pixiv日榜接口的，贼难，不如弃 暗 投 明，直接用360壁纸的api',
         imageUrl: "/immm/flag.jpg",
         timer: 3000,
         showConfirmButton: false
